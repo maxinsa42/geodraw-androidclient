8b309ccefe877bbf310fc17f13fff0d0247a05de
max <maxinsa42@gmail.com>
2016-09-21 06:40:23 -0400

    removed useless directory hierarchy

---
+++ app/src/main/java/fr/insa/ot3/geodrawandroidclient/DrawActivityFragment.java
@@ -0,0 +1,23 @@
+package fr.insa.ot3.geodrawandroidclient;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DrawActivityFragment extends Fragment {
+
+    public DrawActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_draw, container, false);
+    }
+}
